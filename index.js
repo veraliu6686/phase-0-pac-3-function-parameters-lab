e7932a6324315dc9399fe57a888318b951f72b77
lianzi liu <lianziliu.sugar@gmail.com>
2022-09-04 12:41:24 -0400

    commit tree 4eb4a3104584c5f233be3e2764fd5504174b6456

--- index.js
+++ index.js
@@ -10,11 +10,6 @@ function introductionWithLanguage(name, language){
 }
 
 
-function introductionWithLanguage(name, language="JavaScript"){
-    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
-}
-
-
 function introductionWithLanguageOptional(name, language="JavaScript"){
     return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
 }
